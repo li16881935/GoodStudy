9f773c6e8bbe6efd76c55e62845c07b21f6f3e74
pc <274231588@qq.com>
2019-02-18 10:04:54 +0800

    注释

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        // 版本控制系统:VCS, Version Control System
+        // 1.集中式版本控制系统:SVN, subversion
+        // 2.分布式版本控制系统:Git
+
+        // svn服务器:taoCode, SVNChina
+        // Git服务器:gitee, github
+
+        // Git的使用
+
+        // 初始化版本库
+        // git init
+        // 注:当前文件夹中,有文件要进行版本控制
+
+        // 查看当前版本库的状态
+        // git status
+
+        // 指定某个版本进行版本控制
+        // git add
+
+
+
+
+
+    }
+}
