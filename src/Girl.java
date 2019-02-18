48b946fa8837dd9093e19de38ab02e5d958fc679
pc <274231588@qq.com>
2019-02-18 11:17:29 +0800

    添加方法

--- src/Girl.java
+++ src/Girl.java
@@ -5,4 +5,10 @@ public class Girl {
 
     public Girl() {
     }
+
+    public Girl(String name, int age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
 }
